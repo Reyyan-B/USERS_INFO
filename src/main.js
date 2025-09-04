1d5d7bd2c1322f58bf711807bc246147074493fe
Reyyen <reyyen.bebelli@stu.fsm.edu.tr>
2025-09-04 14:18:11 +0300

    fetch and render users dynamically from API

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
-import './style.css'
+const usersAPI = "https://jsonplaceholder.typicode.com/users";
+const usersList = document.querySelector(".users_list");
+const userBox = document.querySelector(".user_box");
+
+async function getUsersNamesId() {
+    const response = await fetch(usersAPI);
+    const data = await response.json();
+
+    data.forEach(user => {
+        const name = document.createElement("h2");
+        name.classList.add("user_name");
+        name.textContent = user.name;
+
+        const id = document.createElement("h3");
+        id.classList.add("user_id");
+        id.textContent = `ID: ${user.id}`;
+
+        const userBox = document.createElement("div");
+        userBox.classList.add("user_box");
+
+        userBox.append(name, id);
+        usersList.append(userBox);
+    });
+}
+
+getUsersNamesId();
+
 
